3b316792fcd04c43d7f544ec0fee5800fd38d0bc
alik-r <alikamran.rza@gmail.com>
2024-05-06 22:00:54 +0400

    Initial commit

---
+++ kdc_chat/core/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import SignupForm
+
+def home(request):
+    return render(request, "core/landing.html")
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignupForm()
+    
+    return render(request, "core/signup.html", {"form": form})
