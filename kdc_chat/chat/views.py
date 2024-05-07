9cee41b24fed9cd4e0363cb53b3da09d4f473459
alik-r <alikamran.rza@gmail.com>
2024-05-07 13:09:16 +0400

    Create chat app and its static templates

---
+++ kdc_chat/chat/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Chat
+
+@login_required
+def chats(request):
+    chats = Chat.objects.all()
+    return render(request, "chat/chats.html", {"chats": chats})
+
+@login_required
+def chat(request, slug):
+    chat = Chat.objects.get(slug=slug)
+    return render(request, "chat/chat.html", {"chat": chat})
